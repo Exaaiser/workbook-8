fdb4b8bd809e16bab9eafd0b0e16bdf12097785d
Exaaiser <umutheturk@gmail.com>
2025-06-09 14:13:42 -0400

    Northwind Database from MYSQL connected to IntelliJ and I can get variables from SQL

---
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root";
+        String password = "umut1453";
+        try {
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Veritabanına başarıyla bağlanıldı!");
+
+
+            String query = "SELECT ProductName FROM products";
+
+
+            Statement statement = connection.createStatement();
+
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            System.out.println("\n--- Northwind Ürünleri ---");
+            while (resultSet.next()) {
+                String productName = resultSet.getString("ProductName");
+                System.out.println(productName);
+            }
+
+            // 6. Kaynakları serbest bırak
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Veritabanı bağlantı hatası!");
+            e.printStackTrace();
+        }
+    }
+}
